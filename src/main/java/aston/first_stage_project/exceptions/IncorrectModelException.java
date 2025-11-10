2ec4bd6c97a25c7ca72c7c3971d6025b016e03a1
Monyakkk <kom.sasha2001@gmail.com>
2025-11-10 22:10:08 +0300

    1) Добавлена возможность выхода из FromFileStrategy в главное меню. 2) Удалены повторяющиеся исключения. 3) Добавлена проверка номера и модели автобуса на длину. 4) Обработано IOException

--- src/main/java/aston/first_stage_project/exceptions/IncorrectModelException.java
+++
@@ -1,9 +0,0 @@
-package aston.first_stage_project.exceptions;
-
-public class IncorrectModelException extends RuntimeException {
-    public IncorrectModelException(String message) {
-        super(message);
-    }
-}
-
-
--- src/main/java/aston/first_stage_project/exceptions/IncorrectRunException.java
+++
@@ -1,7 +0,0 @@
-package aston.first_stage_project.exceptions;
-
-public class IncorrectRunException extends RuntimeException {
-    public IncorrectRunException(String message) {
-        super(message);
-    }
-}
