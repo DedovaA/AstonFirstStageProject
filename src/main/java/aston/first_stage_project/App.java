a05f965733cfa4afef886fe22fce332bdb953c6b
asya <asyatim@mail.ru>
2025-11-12 21:46:09 +0600

    Отредактирован файл App. Добавлена реализация динамического меню. Для этого добавлены классы StrategyMap, MainMenu и во всех стратегиях переопределен метод toString(); Добавлены файлы проверок в resources. Закомментировано закрытие scanner в FromFileStrategy.

--- src/main/java/aston/first_stage_project/App.java
+++ src/main/java/aston/first_stage_project/App.java
@@ -2,45 +2,42 @@ package aston.first_stage_project;
 
 import java.util.List;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
+    private static final DataSource[] strategyArr = {new FromFileStrategy(),new ManuallyStrategy(),new RandomlyStrategy()};
 
     public static void main(String[] args) {
+        Map<String, DataSource> strategyMap = new StrategyMap(strategyArr).getMap();
+        String mainMenuMessage = new MainMenu(strategyArr).getMenu();
         List<Bus> list;
-        try(Scanner scanner = new Scanner(System.in)) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input;
             while (true) {
-                System.out.println("""
-                        Выберите источник ввода данных для сортировки:
-                        1 - из файла,
-                        2 - вручную из консоли,
-                        3 - рандомный список.
-                        Q - для выхода,
-                        и нажмите Enter.""");
-                String userInput = scanner.nextLine();
-                if (userInput.equalsIgnoreCase("Q")) {
+                System.out.println(mainMenuMessage);
+                input = scanner.nextLine().trim();
+                if (input.equalsIgnoreCase("Q")) {
                     System.out.println("Выход из программы.");
                     return;
                 }
-                switch (userInput) {
-                    case "1" -> list = new FromFileStrategy().getBusList();
-                    case "2" -> list = new ManuallyStrategy().getBusList();
-                    case "3" -> list = new RandomlyStrategy().getBusList();
-                    default -> {
-                        System.out.println("Неверный ввод, попробуйте еще раз.");
-                        continue;
+                if (strategyMap.containsKey(input)) {
+                    list = strategyMap.get(input).getBusList();
+//                    if (list != null && !list.isEmpty()) {
+                    if (list != null) {
+                        printResult(list);
+                        break;
                     }
-                }
-                if (list != null) {
-                    printResult(list);
-                    break;
+                } else {
+                    System.out.println("Неверный ввод, попробуйте еще раз.\n");
                 }
             }
         }
     }
 
     private static void printResult(List<Bus> list) {
-        System.out.println("Unsorted list:");
+        System.out.println("\nUnsorted list:");
         list.forEach(System.out::println);
         System.out.println("Sorted list:");
         SortUtils.quickSort(list);
---
+++ src/main/java/aston/first_stage_project/StrategyMap.java
@@ -0,0 +1,20 @@
+package aston.first_stage_project;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StrategyMap {
+    private DataSource[] strategyArr;
+
+    public StrategyMap(DataSource[] strategyArr) {
+        this.strategyArr = strategyArr;
+    }
+
+    Map<String, DataSource> getMap() {
+        Map<String, DataSource> map = new HashMap<>();
+        for (int i = 0; i < this.strategyArr.length; i++) {
+            map.put(String.valueOf(i + 1), this.strategyArr[i]);
+        }
+        return map;
+    }
+}
